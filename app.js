6101b4cf5bcab8fbe0f46a65ff91668dc96a2f2e
Andhika Agung Pratama <79739062+dioptriman@users.noreply.github.com>
2022-12-13 16:47:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ const config = {
     port: process.env.HTTP || 8000,
     allow_origin: "*",
   },
+  trans: {
+    ffmpeg: '/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        dashKeep: true // to prevent dash file delete after end the stream
+      }
+    ]
+  }
 };
 
 var nms = new NodeMediaServer(config);
