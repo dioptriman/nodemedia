9a81d3fbc2aa6afb8bf457636099e110cde46219
Andhika Agung Pratama <79739062+dioptriman@users.noreply.github.com>
2022-12-13 14:12:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,20 @@
-const { NodeMediaServer } = require('node-media-server');
-const express = require('express');
-const mongoose = require('mongoose');
+const NodeMediaServer = require("node-media-server");
 
-const app = express();
-const http = require('http');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const path = require('path');
-const process = require('process')
-const port = process.env.PORT||5000
-const port2 = process.env.PORT||8000
-const port3 = process.env.PORT||8001
+require("dotenv/config");
 
-
-const server = http.createServer(app);
-const io = require('socket.io').listen(server);
-require('./app/controllers/socketIO')(io);
-
-global.appRoot = path.resolve(__dirname);
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-app.set('socketio', io);
-app.set('server', server);
-app.use(express.static(`${__dirname}/public`));
-
-server.listen(port, err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`listening on port ${port}`);
-  }
-});
-
-const nodeMediaServerConfig = {
+const config = {
   rtmp: {
-    port: port2,
+    port: process.env.RTMP || 1935,
     chunk_size: 60000,
     gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
+    ping: 30,
+    ping_timeout: 60,
   },
   http: {
-    port: port3,
-    allow_origin: '*'
-  }
+    port: process.env.HTTP || 8000,
+    allow_origin: "*",
+  },
 };
 
-var nms = new NodeMediaServer(nodeMediaServerConfig);
+var nms = new NodeMediaServer(config);
 nms.run();
